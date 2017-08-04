81aa2b1bf0ce934dc5d0f195bf531d342b5440c4
liyanyan19940914 <921258351@qq.com>
2017-08-04 15:07:59 +0800

    modify mysql connection

--- server/connection.js
+++ server/connection.js
@@ -3,7 +3,7 @@ const mysql = require('mysql');
 let db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '199409',
+    password: '',
     database: 'users',
     port: 3306
 });
